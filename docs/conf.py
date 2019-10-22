61a4b97fb3afc9558e7d521c513b0176be8a11ca
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2019-10-21 20:27:32 -0700

    Remove recommonmark extension

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ release = '0.0.2'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark',
+    # 'recommonmark',
     'sphinx.ext.napoleon'
 ]
 
